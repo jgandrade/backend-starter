fa11f4198ed03f5a60963cbd23c477727179772b
jgandrade <johnglennandrade@gmail.com>
2026-03-13 13:14:02 +0800

    add cors and allow localhost

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.js';
 import invoiceRoutes from './routes/invoices.js';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      const allowed =
+        !origin || /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/.test(origin);
+      cb(null, allowed);
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
